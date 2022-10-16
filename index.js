8ba662c3e78a6fa257bb560487af6cc040197891
neo-kjb <anaszawahreh.dev@gmail.com>
2022-10-16 21:08:02 +0300

    finishing auto-complete widget

--- index.js
+++ index.js
@@ -11,18 +11,48 @@ if (response.data.Error){
 return response.data.Search
 }
 
-const input = document.querySelector('input');
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML=`
+<label><b>search for a movie</b></label>
+<input class="input" />
+<div class ="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results">
+        </div>
+    </div>
+</div>
+`;
+const input = document.querySelector('.input');
+const dropdown = document.querySelector(".dropdown");
+const resultWrapper = document.querySelector('.results');
+
+
 const onInput= async event=>{
     const movies = await fetchData(event.target.value)
+if (!movies.length){
+    dropdown.classList.remove('is-active');
+    return;
+}
+    resultWrapper.innerHTML="";
+    dropdown.classList.add('is-active');
+
     for(let movie of movies){
-        const div = document.createElement('div');
-        div.innerHTML=`
-        <img src="${movie.Poster}">
-        <h1>${movie.Title}</h1>
+        const option = document.createElement('a');
+        const img = movie.Poster=== "N/A"?"":movie.Poster
+        option.classList.add('dropdown-item')
+        option.innerHTML=`
+        <img src="${img}">
+        ${movie.Title}
         `;
-        document.querySelector('#target').appendChild(div)
+        resultWrapper.appendChild(option)
     }
 }
 
 
 input.addEventListener('input',debounce(onInput))
+document.addEventListener('click', (event)=>{
+    if(!root.contains(event.target)){
+        dropdown.classList.remove('is-active');
+    }
+})
